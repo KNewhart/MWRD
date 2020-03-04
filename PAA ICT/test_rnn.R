9cfe148bb60ed6e17b2edccbc5f2024e9ed3f46e
KNewhart <kbnewhart@mines.edu>
2020-03-04 09:50:48 -0700

    Update test_rnn.R

--- PAA ICT/test_rnn.R
+++ PAA ICT/test_rnn.R
@@ -1,112 +1,113 @@
-# Function to create a rnn, train and test in parallel
-rnn <- function(all.data, predict.col, train.obs,
-                act.function="softsign") {
-  # Load required packages
-  pkgs <- c("xts", "dplyr", "parallel", "doSNOW", "foreach")
-  sapply(pkgs, require, character.only = TRUE)
-  if(!("keras" %in% rownames(installed.packages()))) {
-    install.packages("keras")
-    library(keras)
-    keras::install_keras()
-  } else {
-    library(keras)
-  }
-  
-  # How many threads can you run in parallel
-  nThreads<- detectCores(logical = TRUE) 
-  
-  print(paste(Sys.time(),": Starting training..."))
-  
-  # Initialize where each parallel iteration is to be saved
-  all.predictions <- list()
-  # Initialize start and end testing observations
-  start <- train.obs+1
-  end <- start+nThreads*2-1
-  
-  # Start loop
-  while(start < nrow(all.data)) {
+=# Function to create a rnn, train and test in parallel
+  rnn <- function(all.data, predict.col, train.obs,
+                  act.function="softsign") {
+    # Load required packages
+    pkgs <- c("xts", "dplyr", "parallel", "doSNOW", "foreach")
+    sapply(pkgs, require, character.only = TRUE)
+    if(!("keras" %in% rownames(installed.packages()))) {
+      install.packages("keras")
+      library(keras)
+      keras::install_keras()
+    } else {
+      library(keras)
+    }
     
-    if(end == nrow(all.data)) nThreads <- end-start
+    # How many threads can you run in parallel
+    nThreads<- detectCores(logical = TRUE) 
     
-    # Create doSNOW compute cluster
-    cluster = makeCluster(nThreads, type = "SOCK")
-    # register the cluster
-    registerDoSNOW(cluster)
+    print(paste(Sys.time(),": Starting training..."))
     
-    predictions <- foreach(test.obs=seq(start, end), .combine = "rbind", .packages = c("dplyr", "keras")) %dopar% {
-      # Setup training data
-      train.start <- test.obs-train.obs
-      train.end <- test.obs-1
-      # train.data <- all.data[train.start:train.end,] # Not included to save memory
-      # Scale training data
-      train.mean <- apply(all.data[train.start:train.end,],2,mean)
-      train.sd <- apply(all.data[train.start:train.end,],2,sd)
-      # Create 3D array of training data to simulate time sequence in batches of 1
-      train.x <- simplify2array(list(scale(all.data[train.start:train.end,], center=train.mean, scale=train.sd)[,-predict.col]))
-      train.y <- simplify2array(list(scale(all.data[train.start:train.end,], center=train.mean, scale=train.sd)[,predict.col]))
-      n_batch <- dim(train.x)[1]
-      n_epoch <- 1000
-      
-      # Setup model
-      model <- keras_model_sequential() %>%
-        layer_lstm(units=dim(train.x)[2], input_shape=list(dim(train.x)[2],dim(train.x)[3]), activation = act.function) %>%
-        layer_dense(units = 1)
-      model %>% compile(
-        optimizer = optimizer_rmsprop(),
-        loss = "mean_squared_error"
-      )
-      
-      # Train model
-      for(i in seq(1,n_epoch)) {
-        history <- model %>% fit(
-          x=train.x,
-          y=train.y,
-          batch_size=n_batch,
-          epochs=1,
-          shuffle=FALSE, 
-          stateful=TRUE
-        )
-        model %>% reset_states()
-      }
-      
-      # Calculate model fit to training data
-      validation <- model %>% predict(
-        x=train.x,
-        batch_size=1
-      )
-      r2 <- cor(validation, train.y)^2
-      
-      # Setup testing data
-      test.x <- scale(all.data, center=train.mean, scale=train.sd)[test.obs,-predict.col]
-      test.y <- scale(all.data, center=train.mean, scale=train.sd)[test.obs,predict.col]
-      
-      # Predict E coli
-      pred <- model %>% predict(
-        x=array_reshape(test.x, c(1,length(test.x),1)),
-        batch_size=1
-      )
-      
-      # Calculate prediction error (in E. coli units)
-      error <- pred*train.sd[predict.col]+train.mean[predict.col]-as.numeric(test.y*train.sd[predict.col]+train.mean[predict.col])
-      
-      data.frame("R2"=r2, "Prediction.Error"=error)
-    } # parallel loop
-    
-    # stop cluster and remove clients
-    stopCluster(cluster)
-    # insert serial backend, otherwise error in repetetive tasks
-    registerDoSEQ()
-    
-    print(paste(Sys.time(),": Observations", start, "to", end, "completed."))
-    
-    all.predictions[[length(all.predictions)+1]] <- predictions
-    start <- end + 1
-    end <- start + nThreads*2 - 1
+    # Initialize where each parallel iteration is to be saved
+    all.predictions <- list()
+    # Initialize start and end testing observations
+    start <- train.obs+1
+    end <- start+nThreads*2-1
     if(end > nrow(all.data)) end <- nrow(all.data)
-  } # for loop
-  all.predictions <- do.call("rbind", all.predictions)
-  return(all.predictions)
-}
+    
+    # Start loop
+    while(start < nrow(all.data)) {
+      
+      if(end == nrow(all.data)) nThreads <- end-start
+      
+      # Create doSNOW compute cluster
+      cluster = makeCluster(nThreads, type = "SOCK")
+      # register the cluster
+      registerDoSNOW(cluster)
+      
+      predictions <- foreach(test.obs=seq(start, end), .combine = "rbind", .packages = c("dplyr", "keras")) %dopar% {
+        # Setup training data
+        train.start <- test.obs-train.obs
+        train.end <- test.obs-1
+        # train.data <- all.data[train.start:train.end,] # Not included to save memory
+        # Scale training data
+        train.mean <- apply(all.data[train.start:train.end,],2,mean)
+        train.sd <- apply(all.data[train.start:train.end,],2,sd)
+        # Create 3D array of training data to simulate time sequence in batches of 1
+        train.x <- simplify2array(list(scale(all.data[train.start:train.end,], center=train.mean, scale=train.sd)[,-predict.col]))
+        train.y <- simplify2array(list(scale(all.data[train.start:train.end,], center=train.mean, scale=train.sd)[,predict.col]))
+        n_batch <- dim(train.x)[1]
+        n_epoch <- 1000
+        
+        # Setup model
+        model <- keras_model_sequential() %>%
+          layer_lstm(units=dim(train.x)[2], input_shape=list(dim(train.x)[2],dim(train.x)[3]), activation = act.function) %>%
+          layer_dense(units = 1)
+        model %>% compile(
+          optimizer = optimizer_rmsprop(),
+          loss = "mean_squared_error"
+        )
+        
+        # Train model
+        for(i in seq(1,n_epoch)) {
+          history <- model %>% fit(
+            x=train.x,
+            y=train.y,
+            batch_size=n_batch,
+            epochs=1,
+            shuffle=FALSE, 
+            stateful=TRUE
+          )
+          model %>% reset_states()
+        }
+        
+        # Calculate model fit to training data
+        validation <- model %>% predict(
+          x=train.x,
+          batch_size=1
+        )
+        r2 <- cor(validation, train.y)^2
+        
+        # Setup testing data
+        test.x <- scale(all.data, center=train.mean, scale=train.sd)[test.obs,-predict.col]
+        test.y <- scale(all.data, center=train.mean, scale=train.sd)[test.obs,predict.col]
+        
+        # Predict E coli
+        pred <- model %>% predict(
+          x=array_reshape(test.x, c(1,length(test.x),1)),
+          batch_size=1
+        )
+        
+        # Calculate prediction error (in E. coli units)
+        error <- pred*train.sd[predict.col]+train.mean[predict.col]-as.numeric(test.y*train.sd[predict.col]+train.mean[predict.col])
+        
+        data.frame("R2"=r2, "Prediction.Error"=error)
+      } # parallel loop
+      
+      # stop cluster and remove clients
+      stopCluster(cluster)
+      # insert serial backend, otherwise error in repetetive tasks
+      registerDoSEQ()
+      
+      print(paste(Sys.time(),": Observations", start, "to", end, "completed."))
+      
+      all.predictions[[length(all.predictions)+1]] <- predictions
+      start <- end + 1
+      end <- start + nThreads*2 - 1
+      if(end > nrow(all.data)) end <- nrow(all.data)
+    } # for loop
+    all.predictions <- do.call("rbind", all.predictions)
+    return(all.predictions)
+  }
 # Function to compile all data, averaged or instantaneous (TRUE or FALSE)
 compile.data <- function(average.parameters) {
   require(xts)
@@ -200,24 +201,25 @@ compile.data <- function(average.parameters) {
 # Average vs Instantaneous is not significant 
 
 
-# # Test 5-45: Log transform E.coli, Average values, remove NAs from one variable at a time, 90%/10% train/test
-# obj.list <- list()
-# raw.data <- compile.data(average.parameters=TRUE) # Pull data
-# for(i in 6:ncol(raw.data)) {
-#   all.data <- raw.data[-which(is.na(raw.data[,i])),] # Remove NAs from one column
-#   if(nrow(all.data)==0) next
-#   all.data <- all.data[,-which(apply(all.data,2, anyNA))] # Remove columns with NAs
-#   all.data <- apply(all.data, 2, as.numeric)  # Make numberic
-#   ecoli.col <- which(colnames(all.data)=="ECIDX_G") # Select E.coli column
-#   all.data[,ecoli.col] <- log10(all.data[,ecoli.col]) # Log transform E.coli
-#   assign(paste0("test",i+4,".raw"), all.data)
-#   test <- rnn(all.data, predict.col=ecoli.col, train.obs=ceiling(nrow(all.data)*.9))
-#   test[,2] <- 10^test[,2] # Undo log transform
-#   obj.list[[length(obj.list)+1]] <- test
-# }
-# lapply(obj.list, function(t) mean(t[,1])) # R2
-# lapply(obj.list, function(t) mean((t[,2])^2)^0.5) # RMSE
-# 
+# Test 5-45: Log transform E.coli, Average values, remove NAs from one variable at a time, 90%/10% train/test
+rnn.list <- list()
+raw.list <- list() 
+raw.data <- compile.data(average.parameters=TRUE) # Pull data
+for(i in 1:ncol(raw.data)) {
+  all.data <- raw.data[-which(is.na(raw.data[,i])),] # Remove NAs from one column
+  if(nrow(all.data)==0) next
+  all.data <- all.data[,-which(apply(all.data,2, anyNA))] # Remove columns with NAs
+  all.data <- apply(all.data, 2, as.numeric)  # Make numberic
+  ecoli.col <- which(colnames(all.data)=="ECIDX_G") # Select E.coli column
+  all.data[,ecoli.col] <- log10(all.data[,ecoli.col]) # Log transform E.coli
+  raw.list[[length(raw.list)+1]] <- all.data
+  test <- rnn(all.data, predict.col=ecoli.col, train.obs=ceiling(nrow(all.data)*.9))
+  test[,2] <- 10^test[,2] # Undo log transform
+  obj.list[[length(obj.list)+1]] <- test
+}
+unlist(lapply(obj.list, function(t) mean(t[,1]))) # R2
+unlist(lapply(obj.list, function(t) mean((t[,2])^2)^0.5)) # RMSE
+
 # # Test 46-86: Log transform E.coli, Instantaneous values, remove NAs from one variable at a time, 90%/10% train/test
 # raw.data <- compile.data(average.parameters=TRUE) # Pull data
 # for(i in 1:ncol(raw.data)) {
@@ -232,39 +234,8 @@ compile.data <- function(average.parameters) {
 #   obj.list[[length(obj.list)+1]] <- test
 # }
 
-# Test OP: Log transform E.coli, Average values, Remove OP NAs, 90/10 train/test
-all.data <- compile.data(average.parameters=TRUE) # Pull data
-all.data <- all.data[-which(is.na(all.data$OP_FC24)),]
-all.data <- all.data[,-which(apply(all.data,2, anyNA))] # Remove all NAs by column
-all.data <- apply(all.data, 2, as.numeric)  # Make numberic
-ecoli.col <- which(colnames(all.data)=="ECIDX_G") # Select E.coli column
-all.data[,ecoli.col] <- log10(all.data[,ecoli.col])
-testop.raw <- all.data
-testop.rnn <- rnn(all.data, predict.col=ecoli.col, train.obs=ceiling(nrow(all.data)*.9))
 
-# Results: 
-# No log transform: R2 = 0.7974434, mean error = 2861.55
-# Log transform: R2 = 0.7024646, mean error = 1.638332, RMSE = 2.283897
 
-# Test PW: Log transform E.coli, Average values, Remove OP NAs, 90/10 train/test
-all.data <- compile.data(average.parameters=TRUE) # Pull data
-all.data <- all.data[-which(is.na(all.data$PW_FC24)),]
-all.data <- all.data[,-which(apply(all.data,2, anyNA))] # Remove all NAs by column
-all.data <- apply(all.data, 2, as.numeric)  # Make numberic
-ecoli.col <- which(colnames(all.data)=="ECIDX_G") # Select E.coli column
-all.data[,ecoli.col] <- log10(all.data[,ecoli.col])
-testpw.raw <- all.data
-testpw.rnn <- rnn(all.data, predict.col=ecoli.col, train.obs=ceiling(nrow(all.data)*.9))
-
-# Results:
-# No log transform: R2 = 0.5896662, mean error = 4491.2
-# Log transform: R2 = 0.5741033, mean error = 1.26009, RMSE = 1.41097
-
-# Discussion:
-# Log transform is necessary for an accurate prediction
-# PW outperforms OP using mean error and RMSE, but does not fit well (R2). 
-# This is most likely due to the pure number of samples avalible for PW vs OP (277 vs 98)
 
 # Test N: Lag E.coli
 # all.data <- cbind(all.data[2:nrow(all.data),], all.data[1:(nrow(all.data)-1),predict.col]) # lag ecoli
-
