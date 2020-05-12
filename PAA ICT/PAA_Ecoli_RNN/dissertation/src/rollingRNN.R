0a67e23207d59210412a0921172a88a888b8b06a
Newhart, Kate <knewhart@mwrd.dst.co.us>
2020-05-12 09:34:53 -0600

    ecoli rnn

--- PAA ICT/PAA_Ecoli_RNN/dissertation/src/rollingRNN.R
+++ PAA ICT/PAA_Ecoli_RNN/dissertation/src/rollingRNN.R
@@ -1,6 +1,18 @@
+min.max.norm <- function(train, test=NULL) {
+  if(is.vector(train)) train <- data.frame(train)
+  if(is.null(test)) test <- train
+  ma <- apply(train,2,max)
+  mi <- apply(train,2,min)
+  results <- do.call("cbind", lapply(names(ma), function(var) {
+    (test[,var]-mi[var])/(ma[var]-mi[var])
+  }))
+  if(length(rownames(test))>0) rownames(results) <- rownames(test)
+  return(results)
+}
+
 # Function to create a rnn, train and test in parallel/rolling window
 rollingRNN <- function(all.data, predict.col, train.obs,
-                act.function="softsign", n_epoch=500) {
+                act.function="softsign", n_epoch=500, scale=TRUE, n_nodes=NULL) {
   # Load required packages
   pkgs <- c("xts", "dplyr", "parallel", "doSNOW", "foreach")
   sapply(pkgs, require, character.only = TRUE)
@@ -36,26 +48,47 @@ rollingRNN <- function(all.data, predict.col, train.obs,
     registerDoSNOW(cluster)
     
     predictions <- foreach(test.obs=seq(start, end),
-                           .packages = c("dplyr", "keras","zoo")) %dopar% {
+                           .packages = c("dplyr", "keras","zoo"), .export=c("min.max.norm")) %dopar% {
       # Setup training data
       train.start <- test.obs-train.obs
       train.end <- test.obs-1
       # Scale training data
-      train.mean <- apply(all.data[train.start:train.end,],2,mean)
-      train.sd <- apply(all.data[train.start:train.end,],2,sd)
+      # train.mean <- apply(all.data[train.start:train.end,],2,mean)
+      # train.sd <- apply(all.data[train.start:train.end,],2,sd)
       # Create 3D array of training data to simulate time sequence in batches of 1
-      train.x <- scale(all.data[train.start:train.end,], center=train.mean, scale=train.sd)[,-predict.col]
-      if(length(which(apply(train.x,2,anyNA)))>0) train.x <- train.x[,-which(apply(train.x,2,anyNA))]
+      # train.x <- scale(all.data[train.start:train.end,], center=train.mean, scale=train.sd)[,-predict.col]
+      if(scale) train.x <- min.max.norm(all.data[train.start:train.end,-predict.col])
+      if(!scale) train.x <- all.data[train.start:train.end,-predict.col]
+      
+      cols2remove <- which(apply(train.x,2,anyNA))
+      if(length(cols2remove>0)) train.x <- train.x[,-cols2remove]
+      
+      
       train.x <- simplify2array(list(train.x))
       if(length(dim(train.x)) == 3) {
         
-        train.y <- simplify2array(list(scale(all.data[train.start:train.end,], center=train.mean, scale=train.sd)[,predict.col]))
+        train.y <- simplify2array(list(min.max.norm(all.data[train.start:train.end,predict.col])))
         n_batch <- dim(train.x)[1]
+        if(is.null(n_nodes)) n_nodes <- c(dim(train.x)[2], round(dim(train.x)[2]*3))
         
         # Setup model
+        # model <- keras_model_sequential() %>%
+        #   layer_lstm(units=dim(train.x)[2], input_shape=list(dim(train.x)[2],dim(train.x)[3]), activation = act.function) %>%
+        #   layer_dense(units = 1)
         model <- keras_model_sequential() %>%
-          layer_lstm(units=dim(train.x)[2], input_shape=list(dim(train.x)[2],dim(train.x)[3]), activation = act.function) %>%
-          layer_dense(units = 1)
+          layer_lstm(units=n_nodes[1], 
+                      input_shape=list(dim(train.x)[2],dim(train.x)[3]), 
+                      activation = act.function,
+                     return_sequences=TRUE)
+        for(i in 2:length(n_nodes)) {
+          layer_lstm(object=model,
+                      units=n_nodes[i], 
+                      activation = act.function)
+        }
+        layer_dense(object=model, units = 1)
+        
+        
+        
         model %>% compile(
           optimizer = optimizer_rmsprop(),
           loss = "mean_squared_error"
@@ -104,7 +137,10 @@ rollingRNN <- function(all.data, predict.col, train.obs,
         r2 <- cor(validation, train.y)^2
         
         # Setup testing data
-        test.x <- scale(all.data, center=train.mean, scale=train.sd)[test.obs,-predict.col]
+        if(scale) test.x <- min.max.norm(all.data[train.start:train.end,-predict.col],all.data[test.obs,-predict.col] )
+        if(!scale) test <- all.data[test.obs,-predict.col]
+        
+        if(length(cols2remove>0)) test.x <- test.x[-cols2remove]
         # test.x <- test.x[,which(colnames(test.x) %in% colnames(train.x[,,1]))]
         # test.y <- scale(all.data, center=train.mean, scale=train.sd)[test.obs,predict.col]
         
@@ -114,8 +150,11 @@ rollingRNN <- function(all.data, predict.col, train.obs,
           batch_size=1
         )
         
+        if(scale) pred <- pred*(max(all.data[train.start:train.end,predict.col]) - min(all.data[train.start:train.end,predict.col]))+ min(all.data[train.start:train.end,predict.col])
+        
+        
           data.frame("R2"=r2,
-                   "Prediction"=pred*train.sd[predict.col]+train.mean[predict.col],
+                   "Prediction"=pred,
                    "Actual"=all.data[test.obs,predict.col],
                    "Persistance"=all.data[(test.obs-1),predict.col])
       } else {
